bc4063a7e06bc24b5d5b050aa792f16e015cbf47
unknown <argtinfo87@gmail.com>
2024-04-16 13:06:10 -0700

    push Q47 to github

---
+++ main.js
@@ -0,0 +1,29 @@
+/*Q47 - Array Destructuring: Given an array of objects representing different laptops, each with
+properties make, model, and year, use array destructuring to assign the first and second laptops
+ to variables. Then, log these variables.*/
+/*let laptops = [
+   { make: "Dell", model: "XPS 15", year: 2021 },
+   { make: "Apple", model: "MacBook Pro", year: 2020 },
+   { make: "HP", model: "Spectre x360", year: 2021 },
+ ];
+ let [laptop1, laptop2] = laptops;
+ console.log(laptop1);
+ console.log(laptop2);*/
+//Practice the above code.
+/* let laptops = [
+   {make : "Dell", model: "Latitude 2456", year: 2022},
+   {make: "Apple", model : " Macbook pro", year: 2020 },
+   {make: "Hp", model: "Elite book", year:2021},
+  ];
+  let [laptop1,laptop2] = laptops;
+  console.log(laptop1);
+  console.log(laptop2);*/
+//similar code but this time used for mobiles 
+var mobiles = [
+    { make: "Vivo", model: "Y51", year: 2020 },
+    { make: "Apple", model: "iphone 12", year: 2021 },
+    { make: "Samsung", model: "Galaxy S3", year: 2012 },
+];
+var mobile1 = mobiles[0], mobile2 = mobiles[1];
+console.log(mobile1);
+console.log(mobile2);
